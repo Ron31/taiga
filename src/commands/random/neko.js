f621e505168a9ce2e7e50b962ef0bbe22a82a5c3
Ron31 <ron3108@gmx.net>
2019-09-30 20:21:41 +0200

    :recycle: Adjusted Neko Command for updated language system

--- src/commands/random/neko.js
+++ src/commands/random/neko.js
@@ -1,9 +1,8 @@
 const { RichEmbed } = require("discord.js");
-const langs = ["en_us", "de_de"];
 const axios = require("axios");
 
 module.exports.run = async (cmd, client, args, message) => {
-    client.string(message.guild, "command.neko.title", (nekoTitle) => {
+    /*client.string(message.guild, "command.neko.title", (nekoTitle) => {
         client.string(message.guild, "command.neko.footer", (nekoFooter) => {
             client.string(message.guild, "command.neko.nsfw", (nekoNsfw) => {
                 client.string(message.guild, "general.footer", (footer) => {
@@ -27,11 +26,28 @@ module.exports.run = async (cmd, client, args, message) => {
                 });
             });
         });
-    });
+    });*/
+    if(!message.channel.nsfw) {
+        let embed = new RichEmbed()
+            .setTitle(client.config.title + " - " + await client.string(message.guild, "command.neko.title"))
+            .setDescription(await client.string(message.guild, "command.neko.nsfw"))
+            .setColor(client.config.color)
+            .setFooter(client.config.title + " ● " + await client.string(message.guild, "command.neko.footer") + " ● " + (await client.string(message.guild, "general.footer")).replace("$user", message.author.tag));
+        message.channel.send(embed);
+    } else {
+        axios.get("https://nekos.life/api/v2/img/neko").then(async (response) => {
+            let embed = new RichEmbed()
+                .setTitle(client.config.title + " - " + await client.string(message.guild, "command.neko.title"))
+                .setImage(response.data.url)
+                .setColor(client.config.color)
+                .setFooter(client.config.title + " ● " + await client.string(message.guild, "command.neko.footer") + " ● " + (await client.string(message.guild, "general.footer")).replace("$user", message.author.tag));
+            message.channel.send(embed);
+        });
+    }
 };
 
 module.exports.help = {
     name: "neko",
     description: "command.neko.description",
     usage: "command.neko.usage"
-}
+};
