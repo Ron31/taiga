f90fdaed04a6f6ceb6716ed14c2afaacb9dbfe97
Ron31 <ron3108@gmx.net>
2019-09-30 20:22:03 +0200

    :recycle: Adjusted Language Command for updated language system

--- src/commands/misc/language.js
+++ src/commands/misc/language.js
@@ -14,61 +14,41 @@ module.exports.run = async (cmd, client, args, message) => {
         displayError();
     }
 
-    function displayLanguage() {
-        client.string(message.guild, "command.language.title", (languageTitle) => {
-            client.string(message.guild, "command.language.name", (languageName) => {
-                client.string(message.guild, "general.footer", (footer) => {
-                    let embed = new RichEmbed()
-                    .setTitle(client.config.title + " - " + languageTitle + " :globe_with_meridians:")
-                    .setDescription(languageName)
-                    .setColor(client.config.color)
-                    .setFooter(client.config.title + " ● " + footer.replace("$user", message.author.tag));
-                    message.channel.send(embed);
-                });
-            });
-        });
+    async function displayLanguage() {
+        let embed = new RichEmbed()
+            .setTitle(client.config.title + " - " + await client.string(message.guild, "command.language.title") + " :globe_with_meridians:")
+            .setDescription(await client.string(message.guild, "command.language.name"))
+            .setColor(client.config.color)
+            .setFooter(client.config.title + " ● " + (await client.string(message.guild, "general.footer")).replace("$user", message.author.tag));
+        message.channel.send(embed);
     }
-    function displayError() {
-        client.string(message.guild, "command.language.title", (languageTitle) => {
-            client.string(message.guild, "command.language.error", (languageError) => {
-                client.string(message.guild, "general.footer", (footer) => {
-                    let langString = langs.join(", ") 
-                    let embed = new RichEmbed()
-                    .setTitle(client.config.title + " - " + languageTitle)
-                    .setDescription(languageError.replace("$langs", "`" + langString + "`"))
-                    .setColor(client.config.color)
-                    .setFooter(client.config.title + " ● " + footer.replace("$user", message.author.tag));
-                    message.channel.send(embed);
-                });
-            });
-        });
+    async function displayError() {
+        let langString = langs.join(", ")
+        let embed = new RichEmbed()
+            .setTitle(client.config.title + " - " + await client.string(message.guild, "command.language.title"))
+            .setDescription((await client.string(message.guild, "command.language.error")).replace("$langs", "`" + langString + "`"))
+            .setColor(client.config.color)
+            .setFooter(client.config.title + " ● " + (await client.string(message.guild, "general.footer")).replace("$user", message.author.tag));
+        message.channel.send(embed);
     }
     function updateLanguage(langId) {
-        client.string(message.guild, "command.language.success", (languageSucess) => {
-            client.string(message.guild, "command.language.title", (languageTitle) => {
-                client.string(message.guild, "general.error", (error) => {
-                    client.string(message.guild, "general.footer", (footer) => {
-                        client.database.query("UPDATE `guilds_settings` SET `language`=? WHERE `guild` = ? LIMIT 1;", [langId, message.guild.id], (err, results) => {
-                            if(err) {
-                                client.log.error("[G" + message.guild.id + "] Error occured while updating language: " + err, "C_LANGUAGE")
-                                let embed = new RichEmbed()
-                                .setTitle(client.config.title + " - " + languageTitle)
-                                .setDescription(error.replace("$error", "```An error occured while updating MySQL Data. Info will be logged to the console. Please DM Bot administrator for help.```"))
-                                .setColor(client.config.color)
-                                .setFooter(client.config.title + " ● " + footer.replace("$user", message.author.tag));
-                                message.channel.send(embed);
-                            } else {
-                                let embed = new RichEmbed()
-                                .setTitle(client.config.title + " - " + languageTitle)
-                                .setDescription(languageSucess.replace("$lang", "`" + langId + "`"))
-                                .setColor(client.config.color)
-                                .setFooter(client.config.title + " ● " + footer.replace("$user", message.author.tag));
-                                message.channel.send(embed);
-                            }
-                        });
-                    });
-                });
-            });
+        client.database.query("UPDATE `guilds_settings` SET `language`=? WHERE `guild` = ? LIMIT 1;", [langId, message.guild.id], async (err, results) => {
+            if(err) {
+                client.log.error("[G" + message.guild.id + "] Error occured while updating language: " + err, "C_LANGUAGE")
+                let embed = new RichEmbed()
+                    .setTitle(client.config.title + " - " + await client.string(message.guild, "command.language.title"))
+                    .setDescription((await client.string(message.guild, "general.error")).replace("$error", "```An error occured while updating MySQL Data. Info will be logged to the console. Please DM Bot administrator for help.```"))
+                    .setColor(client.config.color)
+                    .setFooter(client.config.title + " ● " + (await client.string(message.guild, "general.footer")).replace("$user", message.author.tag));
+                message.channel.send(embed);
+            } else {
+                let embed = new RichEmbed()
+                    .setTitle(client.config.title + " - " + await client.string(message.guild, "command.language.title"))
+                    .setDescription((await client.string(message.guild, "command.language.success")).replace("$lang", "`" + langId + "`"))
+                    .setColor(client.config.color)
+                    .setFooter(client.config.title + " ● " + (await client.string(message.guild, "general.footer")).replace("$user", message.author.tag));
+                message.channel.send(embed);
+            }
         });
     }
 };
