f19906cb1db9d8ffe857d9adfe96ee7c873c3d56
OfficialCRUGG <ich@crugg.de>
2019-11-05 18:09:21 +0100

    :sparkles: Added "add" function to Inventory

--- src/taiga/Inventory.js
+++ src/taiga/Inventory.js
@@ -23,7 +23,9 @@ class Inventory {
     /**
      * This creates a new Inventory Instance 
      * @param {User} user The user who's inventory should be taken from the database.
+     * @return {Inventory}
      * @static
+     * @async
      */
     static async init(user) {
         return new Promise((resolve, reject) => {
@@ -33,6 +35,22 @@ class Inventory {
                     array.push(await TradingCard.init(results[i]["card"]));
                 }
                 resolve(new Inventory(user, array));
+            });
+        });
+    }
+
+    /**
+     * This adds a trading card to the inventory
+     * @param {TradingCard} card The trading card that should be added to the inventory.
+     * @return {Promise<Boolean>}
+     * @async
+     */
+    async add(card) {
+        return new Promise((resolve, reject) => {
+            database.query("INSERT INTO tc_inventory (user, card) VALUES (?, ?)", [this.user.id. card.id], async(error) => {
+                if(error) throw error;
+                this.tradingCards.push(card);
+                resolve(true);
             });
         });
     }
